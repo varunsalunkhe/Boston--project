5bae35f920fdee6593aedc2c61031c54d3e15a90
Varun Salunkhe <113511064+varunsalunkhe@users.noreply.github.com>
2022-10-30 12:17:31 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+import joblib
+import pandas as pd
+import numpy as np
+from flask import Flask, jsonify, app,  render_template
+from flask import request
+
+app =Flask(__name__)
+
+model = joblib.load("model.pkl")
+
+scale =joblib.load("scale.pkl")
+
+@app.route('/')
+
+def home():
+    return render_template('home.html')
+
+@app.route('/boston-predict', methods=['GET','POST'])
+
+def predict():
+    data = request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data= scale.transform(np.array(list(data.values())).reshape(1,-1))
+    output=model.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+if __name__ == "__main__":
+	app.run(debug= True)
